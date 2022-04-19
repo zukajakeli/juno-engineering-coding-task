f60c995c1ba062ae51e6ca46190b1f3a35cf47ab
ZuJa <zura.j@thekumba.com>
2022-04-19 12:29:32 +0400

    feat: implemented first task

--- src/task1/ImageCarousel.js
+++ src/task1/ImageCarousel.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
+
+import ArrowCircleLeftOutlinedIcon from "@mui/icons-material/ArrowCircleLeftOutlined";
+import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
+import HourglassBottomOutlinedIcon from "@mui/icons-material/HourglassBottomOutlined";
+
 import { fetchImageUrls } from "../api/index";
 
+import "./styles.css";
+
 const ImageCarousel = (props) => {
-    return <h1>Your code goes here</h1>;
+  const [imagesArray, setImagesArray] = useState([]);
+  const [selectedImageNumber, setSelectedImageNumber] = useState(0);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  useEffect(() => {
+    fetchImageUrls().then((res) => {
+      setImagesArray(res);
+    });
+  }, []);
+
+  const selectNextImage = useCallback(() => {
+    setSelectedImageNumber((prev) => {
+      return prev === imagesArray.length - 1 ? 0 : prev + 1;
+    });
+    setIsImageLoaded(false);
+  }, [imagesArray]);
+
+  const selectPrevImage = useCallback(() => {
+    setSelectedImageNumber((prev) => {
+      return selectedImageNumber === 0 ? imagesArray.length - 1 : prev - 1;
+    });
+    setIsImageLoaded(false);
+  }, [imagesArray, selectedImageNumber]);
+
+  const handleImageLoad = useCallback(() => {
+    setIsImageLoaded(true);
+  }, []);
+
+  return (
+    <div className="carousel">
+      <ArrowCircleLeftOutlinedIcon
+        className="prev-button"
+        onClick={selectPrevImage}
+      />
+      {isImageLoaded ? null : <HourglassBottomOutlinedIcon />}
+      <img
+        className={
+          isImageLoaded
+            ? "selected-image"
+            : "selected-image selected-image--hidden"
+        }
+        alt="caroussel"
+        src={imagesArray[selectedImageNumber]}
+        onLoad={handleImageLoad}
+      />
+      <ArrowCircleRightOutlinedIcon
+        className="next-button"
+        onClick={selectNextImage}
+      />
+    </div>
+  );
 };
 export default ImageCarousel;
